ca599d005f281da39845a8fc0b42f4a2479483f1
DBFAlessandro <dbf.alessandro.malusa@gmail.com>
2018-10-21 14:29:02 +0200

    Pulizia codice (import non richiesti etc..) fix test methods

--- src/figura/Main.java
+++ src/figura/Main.java
@@ -1,11 +1,11 @@
 /*
-Dichiarare l’interfaccia “IFigura” con il valore di “pigreco ”
-e i metodi “perimetro() ” e “area() ” . Dichiarare la classe
+Dichiarare l'interfaccia IFigura con il valore di pigreco
+e i metodi perimetro() e area() . Dichiarare la classe
 astratta Figura che implementa IFigura completando il
 metodo perimetro() e non il metodo area(). Figura
 aggiunge il campo pubblico intero nLati, il costruttore che
-assegna la lunghezza di un lato al campo “lato1” e il
-metodo “getNLati()” e gli altri setter e getter.
+assegna la lunghezza di un lato al campo lato1 e il
+metodo getNLati() e gli altri setter e getter.
 Deriva da Figura e codifica le classi concrete
 TriangoloScaleno (aggiungi lato2 e lato3),
 TriangoloEquilatero, Quadrato, Rettangolo, Pentagono
